c476def9dbff0d35135bbdc458e1768a291d8375
lmaaljohare <lma.aref.jo@gmail.com>
2022-04-28 13:18:40 +0300

    Project Skeleton

---
+++ inventory_management/inventory_management/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace inventory_management
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
